459f8de649b5a4bd1d058d244567e3a0624a7f3e
Jan Hertlein <herleinj@gmail.com>
2021-11-27 09:18:58 +0100

    feat: Keyboard Shortcut List

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -12,6 +12,63 @@ import { PicStoreService } from '../services/pic-store.service';
         <img [src]="pic" class="contain"/>
       </pinch-zoom>
     </div>
+    <div *ngIf="!picSet" class="bg-gray-700 w-3/5 mx-auto rounded-lg border border-gray-800 p-6 lg:py-8 lg:px-14 text-gray-300">
+    <div   class="text-white w-full flex relative shadow-sm justify-start mb-10 border-b-2 border-gray-100 border-opacity-20">
+      <div class="mr-4 flex-shrink-0 my-auto">
+        <svg class="fill-current w-8 h-8" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
+            <path  d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
+        </svg>
+      </div>
+      <div class="flex-auto my-2 text-2xl font-bold">
+        <span>Keyboard Actions</span>
+      </div>
+    </div>
+    <div class="w-full grid grid-cols-2 gap-y-2 ">
+      <div class="text-lg font-medium">
+        <div>Show Image</div>
+      </div>
+      <div class="text-lg font-medium text-center justify-self-end">
+        <div>Click</div>
+      </div>
+      <div class="text-lg font-medium flex ">
+        <div class="m-auto ml-0">Datei kopieren</div>
+      </div>
+      <div class="text-lg font-medium text-center justify-self-end flex align-middle">
+        <div class="m-auto ml-0">Press</div><div class="border-gray-100 border-opacity-30 border border-b-2 rounded-lg py-1 px-3 ml-2">C</div>
+      </div>
+      <div class="text-lg font-medium flex">
+        <div class="m-auto ml-0">Compare Image to Current</div>
+      </div>
+      <div class="text-lg font-medium text-center justify-self-end flex">
+        <div class="m-auto ml-0">Hold </div>
+        <div class="border-gray-100 border-opacity-30 border border-b-2 rounded-lg py-1 px-3 mx-2">Strg</div>
+        <div class="m-auto ml-0"> + Click</div>
+      </div>
+      <div class="text-lg font-medium flex">
+        <div class="m-auto ml-0">Show next image</div>
+      </div>
+      <div class="text-lg font-medium text-center justify-self-end flex">
+        <div class="m-auto ml-0">Press</div>
+        <div class="border-gray-100 border-opacity-30 border border-b-2 rounded-lg py-2 px-3 mx-2">
+          <svg  class="fill-current w-5 h-5" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
+              <path fill="currentColor" d="M4,10V14H13L9.5,17.5L11.92,19.92L19.84,12L11.92,4.08L9.5,6.5L13,10H4Z" />
+          </svg>
+        </div>
+      </div>
+      <div class="text-lg font-medium flex">
+        <div class="m-auto ml-0">Show previous image</div>
+      </div>
+      <div class="text-lg font-medium text-center justify-self-end flex">
+        <div class="m-auto ml-0">Press</div>
+        <div class="border-gray-100 border-opacity-30 border border-b-2 rounded-lg py-2 px-3 mx-2">
+          <svg  class="fill-current w-5 h-5" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
+              <path fill="currentColor" d="M20,10V14H11L14.5,17.5L12.08,19.92L4.16,12L12.08,4.08L14.5,6.5L11,10H20Z" />
+          </svg>
+        </div>
+      </div>
+    </div>
+
+    </div>
     `,
   styles: [`
     :host {
