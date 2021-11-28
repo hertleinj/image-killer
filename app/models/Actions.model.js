066148c83291798a61af5760ed19ca768ec3a33e
Jan Hertlein <herleinj@gmail.com>
2021-11-28 07:52:41 +0100

    fix: git remove js from ts for electron

--- app/models/Actions.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Actions = void 0;
-var Actions;
-(function (Actions) {
-    Actions[Actions["COPY"] = 0] = "COPY";
-    Actions[Actions["MOVE"] = 1] = "MOVE";
-})(Actions = exports.Actions || (exports.Actions = {}));
-//# sourceMappingURL=Actions.model.js.map
